e612ac295b3fcda4e3ddede14ec197595faf321c
Igor Starikov <grstrkv@mail.ru>
2022-10-05 14:20:04 +0300

    Update test add_sity

--- tests/test_add_city.py
+++ tests/test_add_city.py
@@ -10,7 +10,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_add_playernotexist(self):
         G = Game(['Moscow'])
-        self.assertFalse(G.add_city('Igor', 'Moscow'))
+        self.assertRaises(ValueError, G.add_city('Igor', 'Moscow'))
 
     def test_add_sitynotexist(self):
         G = Game(['Moscow'])
@@ -19,14 +19,16 @@ class MyTestCase(unittest.TestCase):
 
     def test_add_sitydoubles(self):
         G = Game(['Mathba, Amsterdam'])
-        G.add_player('Igor', 'Ilya')
+        G.add_player('Igor')
+        G.add_player('Ilya')
         G.add_city('Igor', 'Amsterdam')
         G.add_city('Ilya', 'Mathba')
         self.assertFalse(G.add_city('Igor', 'Amsterdam'))
 
     def test_add_wrongsequence(self):
         G = Game(['Mathba, Amsterdam'])
-        G.add_player('Igor', 'Ilya')
+        G.add_player('Igor')
+        G.add_player('Ilya')
         G.add_city('Igor', 'Amsterdam')
         self.assertFalse(G.add_city('Igor', 'Mathba'))
 

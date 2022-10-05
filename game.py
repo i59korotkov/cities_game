b409a29e04e7aafd7063babc08e6b9f9a8a0aed2
Ilya Korotkov <i59korotkov@gmail.com>
2022-10-05 13:04:26 +0300

    Init commit

---
+++ game.py
@@ -0,0 +1,17 @@
+class Game:
+    def __init__(self, cities_list: list) -> None:
+        pass
+    
+    def __city_is_valid(self, prev_city: str, city: str) -> bool:
+        pass
+
+    def add_player(self, player_name: str):
+        pass
+
+    def add_city(self, player_name: str, city: str):
+        pass
+
+    def start(self, player1_name: str, player2_name: str) -> str:
+        pass
+
+    

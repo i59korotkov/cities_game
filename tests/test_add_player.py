e5e4656fa573a0a8be4fdf7b5cf283c0755258f7
Igor Starikov <grstrkv@mail.ru>
2022-10-05 13:55:40 +0300

    test add_player_rework

--- tests/test_add_player.py
+++ tests/test_add_player.py
@@ -5,7 +5,10 @@ from game import Game
 class MyTestCase(unittest.TestCase):
     def test_correct(self):
         G = Game()
-        self.assertRaises(ValueError, G.add_player('Igor'))
+        try:
+            G.add_player('Igor')
+        except ValueError:
+            self.fail("raised ValueError unexpectedly!")
 
     def test_wrong(self):
         G = Game()

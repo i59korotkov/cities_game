958d3afd4d8840b10fff96a0790418b923051719
Ilya Korotkov <i59korotkov@gmail.com>
2022-10-05 13:41:13 +0300

    Wrote code for Game.city_is_valid method

--- game.py
+++ game.py
@@ -1,9 +1,20 @@
 class Game:
     def __init__(self, cities_list: list) -> None:
-        pass
+        self.cities_list = [city.lower() for city in cities_list]
+        self.players = []
+        self.active = False
     
-    def __city_is_valid(self, prev_city: str, city: str) -> bool:
-        pass
+    def city_is_valid(self, prev_city: str, city: str) -> bool:
+        prev_city = prev_city.lower().strip()
+        city = city.lower().strip()
+
+        if city not in self.cities_list:
+            return False
+        
+        if city[0] != prev_city[-1]:
+            return False
+
+        return True
 
     def add_player(self, player_name: str):
         pass
--- tests/test_city_is_valid.py
+++ tests/test_city_is_valid.py
@@ -6,24 +6,24 @@ class TestCityIsValid(unittest.TestCase):
     def test_char_case(self):
         game = Game(['Moscow', 'Perm', 'Warsaw'])
 
-        self.assertTrue(game.__city_is_valid('Moscow', 'waRSaw'))
+        self.assertTrue(game.city_is_valid('Moscow', 'waRSaw'))
 
     def test_spaces(self):
         game = Game(['Moscow', 'Perm', 'Warsaw'])
 
-        self.assertTrue(game.__city_is_valid('Moscow', '   Warsaw   '))
+        self.assertTrue(game.city_is_valid('Moscow', '   Warsaw   '))
 
     def test_repeating(self):
         game = Game(['Moscow', 'Perm', 'Warsaw'])
 
-        self.assertTrue(game.__city_is_valid('Warsaw', 'Warsaw'))
+        self.assertTrue(game.city_is_valid('Warsaw', 'Warsaw'))
 
     def test_diff_char(self):
         game = Game(['Moscow', 'Perm', 'Warsaw'])
 
-        self.assertFalse(game.__city_is_valid('Moscow', 'Perm'))
+        self.assertFalse(game.city_is_valid('Moscow', 'Perm'))
 
     def test_city_not_in_list(self):
         game = Game(['Moscow', 'Perm', 'Warsaw'])
 
-        self.assertFalse(game.__city_is_valid('Perm', 'Madrid'))
+        self.assertFalse(game.city_is_valid('Perm', 'Madrid'))
